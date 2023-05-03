902a1de2f2180d886a755aeb3b2985fed0f29b55
H3BCKN <ka.koszewski@gmail.com>
2023-05-03 15:55:25 +0200

    initial commit

---
+++ minesweeper.rb
@@ -0,0 +1,29 @@
+require 'ruby2d'
+
+GRID = 20 
+WIDTH = 800
+HEIGHT = 800
+BACKGROUND_COLOR = '#ffffff'
+
+
+class Board
+  def initialize
+  end 
+
+  def draw
+  end
+
+
+  private
+  def generate_blocks
+  end 
+end 
+
+
+
+set width: WIDTH
+set height: HEIGHT
+set title: 'minesweeper'
+set background: BACKGROUND_COLOR
+
+show
